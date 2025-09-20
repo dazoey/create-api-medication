f223a70abb8e34793b3e23e6da450593a183bcbd
dazoey <151373427+dazoey@users.noreply.github.com>
2025-09-20 21:53:25 +0700

    update-route-baru

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import dotenv from "dotenv";
 import medicationRoutes from "./routes/medicationRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import supplierRoutes from "./routes/supplierRoutes.js";
+import reportRoutes from "./routes/reportRoutes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-// routes
+//update route baru cuy
 app.use("/api/suppliers", supplierRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/medications", medicationRoutes);
+app.use("/api/reports", reportRoutes);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
